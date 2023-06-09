49bcfff3abdf54e52fb4b096ec2d584955fa44cf
lshahadn <129119994+lshahadn@users.noreply.github.com>
2023-06-09 08:22:30 +0300

    Delete UohdahApp-main/Uohdah directory

--- UohdahApp-main/Uohdah/lib/data/app_data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bustracking/data/models/body/child_info.dart';
-import 'package:bustracking/data/models/body/tracking_data.dart';
-
-List<TrackingData> trackingData = [
-  TrackingData(time: "20:2pm", location: "SA", description: "Test location", date: DateTime.now()),
-  TrackingData(time: "20:2pm", location: "SA", description: "Test location", date: DateTime.now()),
-  TrackingData(time: "20:2pm", location: "SA", description: "Test location", date: DateTime.now()),
-  TrackingData(time: "20:2pm", location: "SA", description: "Test location", date: DateTime.now()),
-];
